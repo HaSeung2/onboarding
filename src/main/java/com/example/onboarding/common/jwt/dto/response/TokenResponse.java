57ff58318517ec59cd349d94f4e58973b4b2cba0
Haseung2 <dlgktmd0@naver.com>
2025-01-18 21:30:39 +0900

    [#19] feat : Swagger 설정

--- src/main/java/com/example/onboarding/common/jwt/dto/response/TokenResponse.java
+++ src/main/java/com/example/onboarding/common/jwt/dto/response/TokenResponse.java
@@ -1,11 +1,14 @@
 package com.example.onboarding.common.jwt.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class TokenResponse {
+    @Schema(name = "token", description = "Access 토큰")
     private final String token;
+    @Schema(name = "refreshToken", description = "refresh 토큰")
     private final String refreshToken;
 
     @Builder
--- src/main/java/com/example/onboarding/domain/user/dto/response/UserSignUpResponse.java
+++ src/main/java/com/example/onboarding/domain/user/dto/response/UserSignUpResponse.java
@@ -1,14 +1,18 @@
 package com.example.onboarding.domain.user.dto.response;
 
 import com.example.onboarding.domain.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class UserSignUpResponse {
+    @Schema(name = "username", description = "아이디")
     private final String username;
+    @Schema(name = "nickname", description = "닉네임")
     private final String nickname;
+    @Schema(name = "authorities", description = "권한 리스트")
     private final List<AuthoritiesResponse> authorities;
 
     public UserSignUpResponse(User user) {
